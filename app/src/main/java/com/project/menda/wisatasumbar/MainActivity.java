b514982a60827f8aae58a48ed1c43544029b3f14
Menda <megifernand@gmail.com>
2017-09-22 15:13:26 +0700

    Show Layout Login when Click Button Visit

--- app/src/main/java/com/project/menda/wisatasumbar/MainActivity.java
+++ app/src/main/java/com/project/menda/wisatasumbar/MainActivity.java
@@ -2,12 +2,55 @@ package com.project.menda.wisatasumbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import mehdi.sakout.fancybuttons.FancyButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout layout;
+    FancyButton btn_visit;
+
+    Animation slideUp;
+    Animation slideDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
+
+        layout = (RelativeLayout) findViewById(R.id.rl_login);
+        btn_visit = (FancyButton) findViewById(R.id.btn_visit);
+
+        btn_visit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                slideUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+                slideDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);
+
+                if (layout.getVisibility() != View.VISIBLE) {
+
+                    layout.startAnimation(slideUp);
+                    layout.setVisibility(View.VISIBLE);
+                    btn_visit.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (layout.getVisibility() == View.VISIBLE) {
+
+            layout.setVisibility(View.GONE);
+            btn_visit.setVisibility(View.VISIBLE);
+        }
+
     }
 }
